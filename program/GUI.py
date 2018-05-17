66fffeee1fbf54f56e2fbbc3ee681acc976e2600
xxjasonx <jasonjason@posteo.de>
2018-05-17 13:14:24 +0200

    first version of program, still missing some options

--- program/GUI.py
+++ program/GUI.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import (QWidget, QGridLayout, QInputDialog,
+from PyQt5.QtWidgets import (QWidget, QGridLayout, QInputDialog, QMessageBox,
 	QPushButton, QApplication, QLabel, QCheckBox, QAction, QMainWindow, QFileDialog, QSizePolicy)
 from PyQt5.QtGui import QFont
 from PyQt5 import QtCore
@@ -28,6 +28,9 @@ class MainWindow(QMainWindow):
 
 		loadFiles = QAction('&load files', self)
 		loadFiles.triggered.connect(self.read_and_generate_cbg)
+	
+		addPrefix = QAction('&add filename prefix for recording', self)
+		addPrefix.triggered.connect(self.addPrefix)
 		
 		self.statusBar()
 			
@@ -35,9 +38,9 @@ class MainWindow(QMainWindow):
 		fileMenu = menubar.addMenu('&File')
 		fileMenu.addAction(changeProject)
 		fileMenu.addAction(loadFiles)
+		fileMenu.addAction(addPrefix)
 		
 		## -- ##
-
 		self.cbg = checkboxgrid()
 		self.setCentralWidget(self.cbg)	
 
@@ -49,7 +52,44 @@ class MainWindow(QMainWindow):
 	
 	def read_and_generate_cbg(self):
 		self.fh.read_all()
-		self.cbg.initCBG(self.fh)
+		self.parser = parser(self.fh)
+		self.cbg.initCBG(self.fh, self.parser)
+
+	def addPrefix(self):
+		if self.cbg.recording == 0:
+			text, ok = QInputDialog.getText(self, 'add prefix to filenames for recording', 'enter prefix:')
+        
+			if ok:
+				self.parser.set_prefix(str(text))
+				print(str(text))
+		else: 
+			QMessageBox.about(self,"add prefix to filenames for recording", "stop Recording first")
+
+	def closeEvent(self, event):
+		try:
+			if self.parser.p_alive.is_alive() == True:
+				self.parser.p_alive.terminate()
+				print("alive message process terminated")
+		except:
+			pass
+		try:
+			if self.parser.p_record.is_alive() == True:
+				self.parser.p_record.terminate()
+				print("record process terminated")
+		except:
+			pass
+		try:
+			if self.parser.p_play.is_alive() == True:
+				self.parser.p_play.terminate()
+				print("play process terminated")
+		except:
+			pass
+		try:
+			for i in range(0,len(self.parser.writefiles)):
+				self.parser.writefiles[i].close()
+			print("files closed")
+		except:
+			pass
 			
 
 class checkboxgrid(QWidget):
@@ -59,21 +99,16 @@ class checkboxgrid(QWidget):
 
 	def initUI(self):
 		self.grid = QGridLayout()
-		self.setLayout(self.grid)
-		#self.grid.setColumnStretch(0,1)
-		#self.grid.setColumnStretch(1,1)
-		#self.grid.setColumnStretch(2,1)
-		#self.grid.setRowStretch(0,1)
-		#self.grid.setRowStretch(1,1)
-		
+		self.setLayout(self.grid)		
 		self.sp = QSizePolicy()
 		self.sp.setHorizontalStretch(0)
 		self.sp.setVerticalStretch(0)
 		
 	
-	def initCBG(self,fh):
+	def initCBG(self, fh, parser):
 		
-		self.parser = parser(fh)
+		self.fh = fh
+		self.parser = parser
 
 		## CHANGE RENDERER ##
 
@@ -92,17 +127,34 @@ class checkboxgrid(QWidget):
 		self.pan.stateChanged.connect(lambda checked: self.click_pan(self.pan))
 		self.ssr.stateChanged.connect(lambda checked: self.click_ssr(self.ssr))
 		
-		## PLAY AND PREPARE PLAY BUTTON ##
+		## PLAY AND RECORD BUTTONS ##
+
+		self.playing = 0
+		self.recording = 0
 
 		self.prepare_play = QPushButton('prepare playing', self)
 		self.prepare_play.setSizePolicy(self.sp)
+		self.prepare_play.setStyleSheet("background-color: white")
 		self.grid.addWidget(self.prepare_play, 1, 0)
 		self.prepare_play.clicked.connect(self.prepare_play_pressed)
 
 		self.play = QPushButton('PLAY', self)
 		self.play.setSizePolicy(self.sp)
+		self.play.setStyleSheet("background-color: white")
 		self.grid.addWidget(self.play, 1, 1)
 		self.play.clicked.connect(self.play_pressed)	
+
+		self.prepare_rec = QPushButton('prepare recording', self)
+		self.prepare_rec.setSizePolicy(self.sp)
+		self.prepare_rec.setStyleSheet("background-color: white")
+		self.grid.addWidget(self.prepare_rec, 1, 2)
+		self.prepare_rec.clicked.connect(self.prepare_rec_pressed)
+
+		self.rec = QPushButton('REC', self)
+		self.rec.setSizePolicy(self.sp)
+		self.rec.setStyleSheet("background-color: white")
+		self.grid.addWidget(self.rec, 1, 3)
+		self.rec.clicked.connect(self.rec_pressed)
 
 
 		## TABLE WITH CHECKBOXES ##
@@ -119,14 +171,14 @@ class checkboxgrid(QWidget):
 		self.grid.addWidget(textPLAY, 2,1)
 		self.grid.addWidget(textREC, 2,2)
 
-		numbers = fh.ID #source ID list from filehandler
+		numbers = self.fh.ID #source ID list from filehandler
 		N = len(numbers)
 		
 		for i in range(0,N):
 			numbers[i] = str(numbers[i])
 		
-		pcb = [] #PLAY checkboxes
-		rcb = [] #REC checkboxes
+		self.pcb = [] #PLAY checkboxes
+		self.rcb = [] #REC checkboxes
 		binary_list = [0] * N 
 		#information about which sources to play and record
 		self.parser.sources_to_play=binary_list
@@ -138,8 +190,8 @@ class checkboxgrid(QWidget):
 			#generate names for sourcenumber label-widgets
 			self.labelnames.append(str(i))
 			#generate names for play and record checkbox-widgets
-			pcb.append(str(pcb) + str(i))
-			rcb.append(str(rcb) +str(i))
+			self.pcb.append(str(self.pcb) + str(i))
+			self.rcb.append(str(self.rcb) +str(i))
 
 		for i in range (0,N):
 
@@ -148,15 +200,15 @@ class checkboxgrid(QWidget):
 			self.grid.addWidget(self.labelnames[i], i+3,0)
 			
 
-			pcb[i] = QCheckBox('')
-			pcb[i].setSizePolicy(self.sp)
-			self.grid.addWidget(pcb[i], i+3, 1)
-			pcb[i].stateChanged.connect(lambda checked, i=i: self.click_pcb(pcb[i], numbers[i], i))
+			self.pcb[i] = QCheckBox('')
+			self.pcb[i].setSizePolicy(self.sp)
+			self.grid.addWidget(self.pcb[i], i+3, 1)
+			self.pcb[i].stateChanged.connect(lambda checked, i=i: self.click_pcb(self.pcb[i], numbers[i], i))
 			
-			rcb[i] = QCheckBox('')
-			rcb[i].setSizePolicy(self.sp)
-			self.grid.addWidget(rcb[i], i+3, 2)
-			rcb[i].stateChanged.connect(lambda checked, i=i: self.click_rcb(rcb[i], numbers[i], i))
+			self.rcb[i] = QCheckBox('')
+			self.rcb[i].setSizePolicy(self.sp)
+			self.grid.addWidget(self.rcb[i], i+3, 2)
+			self.rcb[i].stateChanged.connect(lambda checked, i=i: self.click_rcb(self.rcb[i], numbers[i], i))
 
 			self.grid.setRowStretch(i+2,1)
 
@@ -188,6 +240,7 @@ class checkboxgrid(QWidget):
 			print("playing of source " + str(num_i) + " got deactivated")
 		print('play: ')
 		print (self.parser.sources_to_play)
+
 	def click_rcb(self, box_id,num_i,i):
 		if box_id.isChecked():
 			self.parser.sources_to_record[i]=num_i
@@ -198,15 +251,61 @@ class checkboxgrid(QWidget):
 		print('record: ')
 		print (self.parser.sources_to_record)
 
-	def play_pressed(self):	
-	
-		self.parser.play()
-		
 
 	def prepare_play_pressed(self):
 		self.parser.prepare_play()
 		self.prepare_play.setStyleSheet("background-color: yellow")
+		for i in range(0,len(self.fh.ID)):
+			if str(self.fh.ID[i]) in self.parser.sources_to_play:
+				self.pcb[i].setStyleSheet("background-color: yellow")
+			else: 
+				self.pcb[i].setStyleSheet("background-color: white")
+		
 		print('ready for playing')
+
+	def play_pressed(self):	
+		if self.playing== 0:
+			self.parser.play()
+			self.playing = 1
+			self.play.setStyleSheet("background-color: green")
+			print("playing ... ")
+		else:
+			self.playing = 0
+			self.play.setStyleSheet("background-color: white")
+			self.parser.p_play.terminate()
+			print("playing stopped")
+
+	def prepare_rec_pressed(self):
+		self.parser.prepare_record()
+		self.prepare_rec.setStyleSheet("background-color: yellow")
+		for i in range(0,len(self.fh.ID)):
+			if str(self.fh.ID[i]) in self.parser.sources_to_play:
+				self.rcb[i].setStyleSheet("background-color: yellow")
+			else: 
+				self.rcb[i].setStyleSheet("background-color: white")
+		print('ready for recording')
+
+	def rec_pressed(self):
+		if self.recording == 0:
+			self.parser.record()
+			self.recording = 1
+			self.rec.setStyleSheet("background-color: red")
+			print("recording ...")
+		else:
+			self.recording = 0
+			self.rec.setStyleSheet("background-color: white")
+			self.parser.p_record.terminate()
+			print("recording stopped")
+			self.parser.close_files()
+			print("files closed")
+		
+
+
+
+
+
+
+
 			
 if __name__ == '__main__':
 
--- program/classes.py
+++ program/classes.py
@@ -7,6 +7,7 @@ from os import listdir
 from os.path import isfile, join
 from pythonosc import osc_message_builder as omb
 from OSCcodec import decodeOSC
+from multiprocessing import Process
 
 
 
@@ -42,12 +43,9 @@ class filehandler(object):
 			
 			self.ID.append(int(filecontent[0]))
 		
-		print(self.ID)
-		print(oscFiles)
 		#sort oscFiles list in the same way as source ID list
 		self.ID, oscFiles = (list(k) for k in zip(*sorted(zip(self.ID, oscFiles))))
-		print(self.ID)
-		print(oscFiles)
+
 
 		for i in oscFiles:
 			try:
@@ -67,6 +65,7 @@ class filehandler(object):
 				self.z.append(zlist)				
 				print('no z-coordinate given in ' + str(i))
 		print("loaded " + str(N) + " source position files")
+
 		
 class parser(filehandler):
 
@@ -83,15 +82,38 @@ class parser(filehandler):
 		self.renderer = "panoramix"
 		self.ip = '127.0.0.1'
 		self.port = 4002
+		self.recv_port = 4001 # stays the same as no reveive port is necessary for SSR
 	
 		#create socket for UDP connection
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		#second socket for receiving OSC
+		self.recv_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.recv_sock.bind((self.ip,self.recv_port))
 		
 		# binary list of sources to be played (linked to checkboxgrid in GUI)
 		self.sources_to_play = []
+		self.sources_to_record =[]
+		
+		# OSC address lists for Panoramix
+		self.OSCrecord_list_dist = []
+		self.OSCrecord_list_elev = []
+		self.OSCrecord_list_azim = []
 		
 		#jack server
-		self.jclient = jack.Client('jack1')
+		self.jclient = jack.Client('jackOSCparser')
+		self.start_jack()
+
+		#files to write to
+		
+		self.writefiles = []
+		self.filenames_w =[]
+		self.filesopen_w = []
+		self.prefix =""	
+		for i in range(0,len(self.ID)):
+			self.writefiles.append(self.ID[i])
+			self.filenames_w.append(self.prefix +"pos"+str(self.ID[i]))
+			self.filesopen_w.append(0) 	
+		
 
 	def change_renderer(self, flag):
 		# flag = 0 -> Panoramix
@@ -103,11 +125,14 @@ class parser(filehandler):
 			self.port=50001
 
 	def sendOSC(self, msg):
-		self.sock.sendto(msg.dgram, (self.ip, self.port))
+		self.sock.sendto(msg.dgram, (self.ip, self.port))			
 
 	def connect(self):
 		if self.renderer is "panoramix":
-			pass
+			try:
+				self.p_alive.terminate()
+			except:
+				pass
 		if self.renderer is "ssr":
 			#subscribe to SSR (SSR needs to be SERVER)
 			msg1 = omb.OscMessageBuilder(address="/subscribe")
@@ -120,6 +145,9 @@ class parser(filehandler):
 			msg2=msg2.build()
 			self.sendOSC(msg2)
 			# need to send alive messages after subscription
+			# do multiprocessing
+			self.p_alive = Process(target = self.send_alive_messages)
+			self.p_alive.start()
 
 	# only necessary for SSR communication
 	def send_alive_messages(self):
@@ -128,6 +156,7 @@ class parser(filehandler):
 			msg = msg.build()
 			while 1:
 				self.sendOSC(msg) 
+				print("sent /alive message to SSR")
 				time.sleep(0.5)
 		else:
 			pass
@@ -218,7 +247,9 @@ class parser(filehandler):
 		self.create_sources()
 
 		
-	def play(self):
+	def _play(self):
+		#TO DO terminate this process when playing is stopped
+
 		ix = 0 # index for iterating through lines from files for the sources to be played
 		l_ix = len(self.tt)
 		
@@ -243,6 +274,9 @@ class parser(filehandler):
 			if ix == l_ix:
 				print('all values played')
 				break
+	def play(self):
+		self.p_play = Process(target = self._play)
+		self.p_play.start()
 
 	def send_position_pan(self, num, dist, az, el): 
 		add = "/track/" +str(int(num))+ "/dist"
@@ -274,7 +308,98 @@ class parser(filehandler):
 		msg.add_arg(y)
 		msg=msg.build()
 		self.sendOSC(msg)
-		print("sent "+ add + " " + str(int(num)) + " " +str(x) + " " + str(y) +" to SSR ( " + self.ip + " port: " + str(self.port) + " )")	
+		print("sent "+ add + " " + str(int(num)) + " " +str(x) + " " + str(y) +" to SSR ( " + self.ip + " port: " + str(self.port) + " )")
+
+	def prepare_record(self):
+		self.OSCrecord_list_dist = []
+		self.OSCrecord_list_elev = []
+		self.OSCrecord_list_azim = []	
+		for i in range(0,len(self.sources_to_record)):
+			if self.sources_to_record[i] != 0:
+				# adding OSC addresses that should be recorded
+				self.OSCrecord_list_dist.append("/track/" + self.sources_to_record[i] +"/dist")
+				self.OSCrecord_list_elev.append("/track/" + self.sources_to_record[i] +"/elev")
+				self.OSCrecord_list_azim.append("/track/" + self.sources_to_record[i] +"/azim")
+				# open files to record to if not yet open
+				if self.filesopen_w[i] == 0:
+					self.writefiles[i] = open(self.filenames_w[i],"w")
+					self.filesopen_w[i] = 1
+			else:
+				# adding empty strings if there's nothing to record
+				self.OSCrecord_list_dist.append("--")
+				self.OSCrecord_list_elev.append("--")
+				self.OSCrecord_list_azim.append("--")
+				# close files that are not about to be recorded
+				if self.filesopen_w[i] ==1:
+					self.writefiles[i].close()
+		print(self.writefiles)
+		print(self.filenames_w)
+		print(self.filesopen_w)
+								
+				
+		
+	def _record(self):
+		# no info at the beginning.... #TO DO in play: skip lines where NaN appears
+		dist = np.nan
+		elev = np.nan
+		azim = np.nan
+		while 1:
+			data1, addr = self.recv_sock.recvfrom(2056) # buffer size is 2056 bytes
+			data1 = decodeOSC(data1)
+			if data1[0] in self.OSCrecord_list_dist:
+				i = self.OSCrecord_list_dist.index(data1[0])
+				dist = data1[2]
+				self.writefiles[i].write("positions\t"+str(self.jclient.transport_frame)+"\t"+str(self.ID[i])+"\t"+str(dist)+"\t"+str(elev)+"\t"+str(azim)+"\n")
+				self.writefiles[i].flush()
+				print(data1)
+				data1 = "-"
+			elif data1[0] in self.OSCrecord_list_azim:
+				i =self.OSCrecord_list_azim.index(data1[0])
+				azim = data1[2]
+				self.writefiles[i].write("positions\t"+str(self.jclient.transport_frame)+"\t"+str(self.ID[i])+"\t"+str(dist)+"\t"+str(elev)+"\t"+str(azim)+"\n")
+				self.writefiles[i].flush()
+				print(data1)
+				data1 = "-"
+			elif data1[0] in self.OSCrecord_list_elev:
+				i=self.OSCrecord_list_elev.index(data1[0])
+				elev =data1[2]
+				self.writefiles[i].write("positions\t"+str(self.jclient.transport_frame)+"\t"+str(self.ID[i])+"\t"+str(dist)+"\t"+str(elev)+"\t"+str(azim)+"\n")
+				self.writefiles[i].flush()
+				print(data1)
+				data1 = "-"
+							
+
+	def record(self):
+
+		self.p_record = Process(target = self._record)
+		self.p_record.start()
+
+	def close_files(self):
+		for i in range(0, len(self.writefiles)):
+			try:
+				self.writefiles[i].close()
+				self.filesopen_w[i] = 0
+			except:
+				pass
+
+	def set_prefix(self, text):
+		self.prefix = text
+		for i in range(0,len(self.ID)):
+			string = self.prefix +"pos"+str(self.ID[i])
+			self.filenames_w[i]= string
+
+		print(self.filenames_w)
+			
+			
+		
+		
+
+		
+		
+
+	
+
+	
 
 		
 		
