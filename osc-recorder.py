59b036beff3b00a0bae2bbe25b51a66f8e7447f8
Yrkkö Äkkijyrkkä <akkijyrkkae@campus.tu-berlin.de>
2019-12-17 12:18:29 +0100

    Changed to system time with generic handler - for UserStudy!

--- osc-recorder.py
+++ osc-recorder.py
@@ -4,6 +4,7 @@
 __author__ = "Henrik von Coler"
 __date__   = "2019-06-07"
 
+import time
 
 import argparse
 import jack
@@ -12,75 +13,108 @@ import os
 from pythonosc import dispatcher
 from pythonosc import osc_server
 
+from typing import List, Any
 
-##############################################################################
-##############################################################################
 
-def volume_handler(unused_addr, ch1, ch2, gain, timestamp):
+def generic_handler(unused_addr, *oscArgs: List[Any]):
     
-  f = open('gains', 'a')
-  f.write("gain")
-  f.write("\t")
-  #f.write(str(timestamp))
-  f.write(str(client.transport_frame / client.samplerate))
-  f.write("\t")
-  f.write(str(ch1))
-  f.write("\t") 
-  f.write(str(ch2))
-  f.write("\t")
-  f.write(str(gain))
-  f.write("\n")
+        """Generic handler for all OSC paths (addresses)."""
+        
+        fileString =  unused_addr.replace('/','_') + ".osc"
+        
+        n_arguments = len(oscArgs)
+        
+        f       = open(args.outpath + fileString, 'a')
     
-#------------------------------------------------------------------------------
-
-def handler_wonder(unused_addr, ID, x, y, duration):
-
-  tmpName = args.outpath + "WONDER_" + str(ID) + "_cartesian" + ".osc";
-  
-  f       = open(tmpName, 'a')
-  
-  timeStamp = client.transport_frame / client.samplerate;
-
-  f.write('%.4f' % (timeStamp))
-  f.write("\t")
-
-  f.write(unused_addr)
-  f.write("\t")
-  
-  f.write(str(ID))
-  f.write("\t")
-
-  f.write('%.4f' % (x))
-  f.write("\t")
-
-  f.write('%.4f' % (y))
-  f.write("\t")
-
-  f.write('%.4f' % (duration))
-  f.write("\n")
+        timeStamp = time.time()
+        
+        f.write('%.12f' % (timeStamp))    
+        f.write("\t")
     
-#------------------------------------------------------------------------------
-  
-def handler_polar_single(unused_addr, value):
-    #
-    """ Designed to process PanoramixApp messages. """
+        f.write(unused_addr)       
+            
+        for i in range(n_arguments):
     
-    [o, t, i, p] = unused_addr.split("/")
-    
-    tmpName = "track_"+str(i)+'_'+str(p)+".osc";
-    f       = open(args.outpath + tmpName, 'a')
-
-    timeStamp = client.transport_frame / client.samplerate;
-
-    f.write('%.4f' % (timeStamp))
-    f.write("\t")
-
-    f.write(unused_addr)
-    f.write("\t")
-  
-    f.write(str(value))
-    f.write("\n")
-
+            tmpArg = oscArgs[i]        
+            
+            # this could be needed for checking types
+            # print(type(tmpArg))
+            
+            f.write("\t")
+              
+            f.write(str(tmpArg))
+            
+            
+        f.write("\n")
+#        
+###############################################################################
+###############################################################################
+#
+#def volume_handler(unused_addr, ch1, ch2, gain, timestamp):
+#    
+#  f = open('gains', 'a')
+#  f.write("gain")
+#  f.write("\t")
+#  #f.write(str(timestamp))
+#  f.write(str(client.transport_frame / client.samplerate))
+#  f.write("\t")
+#  f.write(str(ch1))
+#  f.write("\t") 
+#  f.write(str(ch2))
+#  f.write("\t")
+#  f.write(str(gain))
+#  f.write("\n")
+#    
+##------------------------------------------------------------------------------
+#
+#def handler_wonder(unused_addr, ID, x, y, duration):
+#
+#  tmpName = args.outpath + "WONDER_" + str(ID) + "_cartesian" + ".osc";
+#  
+#  f       = open(tmpName, 'a')
+#  
+#  timeStamp = client.transport_frame / client.samplerate;
+#
+#  f.write('%.4f' % (timeStamp))
+#  f.write("\t")
+#
+#  f.write(unused_addr)
+#  f.write("\t")
+#  
+#  f.write(str(ID))
+#  f.write("\t")
+#
+#  f.write('%.4f' % (x))
+#  f.write("\t")
+#
+#  f.write('%.4f' % (y))
+#  f.write("\t")
+#
+#  f.write('%.4f' % (duration))
+#  f.write("\n")
+#    
+##------------------------------------------------------------------------------
+#  
+#def handler_polar_single(unused_addr, value):
+#    #
+#    """ Designed to process PanoramixApp messages. """
+#    
+#    [o, t, i, p] = unused_addr.split("/")
+#    
+#    tmpName = "track_"+str(i)+'_'+str(p)+".osc";
+#    f       = open(args.outpath + tmpName, 'a')
+#
+#    timeStamp = client.transport_frame / client.samplerate;
+#
+#    f.write('%.4f' % (timeStamp))
+#    f.write("\t")
+#
+#    f.write(unused_addr)
+#    f.write("\t")
+#  
+#    f.write(str(value))
+#    f.write("\n")
+#
 
         
 ##############################################################################
@@ -110,22 +144,24 @@ if __name__ == "__main__":
     
   
   dispatcher = dispatcher.Dispatcher()  
-    
-  dispatcher.map("/gain/", volume_handler )
   
-  dispatcher.map("/WONDER/source/position", handler_wonder )
+  dispatcher.map("/*", generic_handler )
   
-  for i in range(1,64):
-      
-      tmpStr = "/track/"+str(i)+"/azim"
-      dispatcher.map(tmpStr, handler_polar_single)
-    
-      tmpStr = "/track/"+str(i)+"/dist"
-      dispatcher.map(tmpStr, handler_polar_single)
-      
-      tmpStr = "/track/"+str(i)+"/elev"
-      dispatcher.map(tmpStr, handler_polar_single)
-      
+#  dispatcher.map("/gain/", volume_handler )
+#  
+#  dispatcher.map("/WONDER/source/position", handler_wonder )
+#  
+#  for i in range(1,64):
+#      
+#      tmpStr = "/track/"+str(i)+"/azim"
+#      dispatcher.map(tmpStr, handler_polar_single)
+#    
+#      tmpStr = "/track/"+str(i)+"/dist"
+#      dispatcher.map(tmpStr, handler_polar_single)
+#      
+#      tmpStr = "/track/"+str(i)+"/elev"
+#      dispatcher.map(tmpStr, handler_polar_single)
+#      
   
   server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
   print("Serving on {}".format(server.server_address))
