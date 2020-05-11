222340a42bd82f8594085a12bb55972f21d7c863
Patric Schmitz <bzk0711@aol.com>
2020-05-11 22:40:16 +0200

    move executable scripts to root directory

---
+++ osc-recorder.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+""" Main file for starting an OSC recorder from command line. """
+
+__author__ = "Henrik von Coler"
+__date__   = "2019-06-07"
+
+import argparse
+
+import sys
+
+from py_osc_seq import OscRecorder
+
+#########################################################################
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--timer", default ="unix",
+                        help="chose timer mode")
+    parser.add_argument("--outpath", default ="test-record",
+                        help="filename for ouput")
+    parser.add_argument("--ip", default="127.0.0.1",
+                        help="The ip to listen on")
+    parser.add_argument("--port", type=int, default=5005,
+                        help="The port to listen on")
+
+    args = parser.parse_args()
+
+    r = OscRecorder(args.timer)
+    r.start_server(args.ip, args.port, args.outpath)
